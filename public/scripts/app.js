49137caefd5e380a80df25014a47b050c1b1044b
Andrew <andrew.j.hui@gmail.com>
2019-09-11 06:08:24 +0000

    tweeter updates with iterating through data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,106 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// /*
+//  * Client-side JS logic goes here
+//  * jQuery is already loaded
+//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+//  */
+
+// // let $ = require('jQuery');
+
+
+
+
+
+// // testObject =  {
+// //   "user": {
+// //     "name": "Newton",
+// //     "avatars": "https://i.imgur.com/73hZDYK.png",
+// //       "handle": "@SirIsaac"
+// //     },
+// //   "content": {
+// //       "text": "If I have seen further it is by standing on the shoulders of giants"
+// //     },
+// //   "created_at": 1461116232227
+// // }
+
+
+// // console.log(createTweetElement(testObject))
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+const renderTweets = (tweetData) => {
+  for (let par of tweetData) {
+    const $htmlCode = createTweetElement(par);
+    $('#tweet-container').append($htmlCode);
+  }
+};
+
+const createTweetElement = function(tweetObject) {
+  //returns a tweet article element containing the entire HTML structure of the tweet
+  const {name, avatars, handle} = tweetObject.user;
+  const {text} = tweetObject.content;
+  const {created_at} = tweetObject;
+  
+  return `<article class ="tweets-container">
+  <header class="tweetsBox"> 
+
+    <img class=tweetImage src='${avatars}'> </img>
+    
+    <textarea class="nameTweet" name="text">
+      ${name}
+    </textarea>
+    <textarea class="contentTweet" name="content">
+        ${text}
+      </textarea>
+      <div class="borderUnder">
+        
+      </div>
+
+      <div class="handle">
+      ${handle}
+      </div>
+      
+      <div class="daysLeft">
+        ${created_at}
+      </div>
+
+    </header>
+</article>`
+}
+
+$(document).ready(function(){
+
+  renderTweets(tweetData);
+
+});
+
+
+// Test / driver code (temporary)
+ // to see what it looks like
+ // to add it to the page so we can make sure it's got all 
 
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function() {
+  // console.log("SDFA")
+  // const words = $("textarea").keypress(function() {
+  //   let inputText = this.value;
+  //   console.log(inputText, inputText.length)
+    
+  //   return inputText;
+  // })
+
+  $("textarea").on('input', function() {
+    let words = $(this).val()
+    // if (words.length > 140) { 
+    // }  
+    const counter = $(this).siblings(".counter");
+
+    let num = 140 - words.length
+
+    counter.text(num)
+
+    if (num < 0) {
+      counter.addClass("counter over")
+    }
+    // else if (num > -1 && counter.find("counter over")) {
+    //   counter.removeClass("counter over") // not working
+    // }
+
+    
+  })
+
+  // --- our code goes here ---
+});
+
